6a646b3f138965b8ecdbcf6375144896fdf449ab
toyoko-creators <70806719+toyoko-creators@users.noreply.github.com>
2020-12-08 19:30:55 +0900

    Add files via upload

---
+++ public/templates/header.php
@@ -0,0 +1,14 @@
+<html lang="ja">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewpoint" content="width=device-width">
+	<title><?php  echo $pagetitle; ?></title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/slick.css">
+    <link rel="stylesheet" type="text/css" href="css/slick-theme.css">
+	<meta name="viewpoint" content="width=device-width">
+	<title><?php  echo $pagetitle; ?></title>
+	<link rel="stylesheet" href="css/style.css">
+</head>
