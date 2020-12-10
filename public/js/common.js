9d71da0e9f00fbffe30bf0222bf976d80322c5ca
toyoko-creators <70806719+toyoko-creators@users.noreply.github.com>
2020-12-10 19:24:50 +0900

    Update common.js

--- public/js/common.js
+++ public/js/common.js
@@ -2,11 +2,24 @@ $('.slick01').slick(
 {
     centerMode: true,
     centerPadding: '20%',
-    dots: true,
+    // dots: true,
     variableWidth: true,
     slidesToShow: 1,
-    // prevArrow: true,
-    // nextArrow: true,
+    slidesToScroll: 1,
+    infinite: false,      // スライドをループしない
+    focusOnSelect: true,  // 中央以外をクリックすると、中央まで移動(=フォーカス)する。
     
   });
 
+  $('.slick02').slick(
+    {
+        centerMode: true,
+        centerPadding: '20%',
+        // dots: true,
+        variableWidth: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: false,      // スライドをループしない
+        focusOnSelect: true,  // 中央以外をクリックすると、中央まで移動(=フォーカス)する。
+        
+    });
