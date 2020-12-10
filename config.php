24e7412710075d848181f9679a7fed8f01423512
toyoko-creators <70806719+toyoko-creators@users.noreply.github.com>
2020-12-10 21:56:43 +0900

    Update config.php

--- config.php
+++ config.php
@@ -9,7 +9,7 @@ $host       = "127.0.0.1";
 $username   = "homestead";
 $password   = "secret";
 $dbname     = "homestead";
-$dsn        = "mysql:host=$host;dbname=$dbname;port=3306";
+$dsn        = "mysql:host=$host;dbname=$dbname";
 //$dsn        = "mysql:host=$host;dbname=$dbname";
 $$options    = array(
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
